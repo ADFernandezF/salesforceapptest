a8f4710577f87b186b6de3c61c4e13a5ba59500c
Alberto Mac <cort.084@gmail.com>
2020-08-27 20:13:13 +0200

    ADD New LWC to show all trades

---
+++ force-app/main/default/lwc/newTrade/newTrade.js
@@ -0,0 +1,23 @@
+import { LightningElement, track, wire } from 'lwc';
+import tradeController from '@salesforce/apex/TradeController.getTrades';
+
+export default class NewTrade extends LightningElement {
+
+	@track columns = [
+		{ label: 'ID', fieldName: 'Name', hideDefaultActions: 'true' },
+		{ label: 'Sell Currency', fieldName: 'SellCurrency__c', type: 'text', hideDefaultActions: 'true' },
+		{ label: 'Sell Amount', fieldName: 'SellAmount__c', type: 'number', hideDefaultActions: 'true' },
+		{ label: 'Buy Currency', fieldName: 'BuyCurrency__c', type: 'text', hideDefaultActions: 'true' },
+		{ label: 'Buy Amount', fieldName: 'BuyAmount__c', type: 'number', hideDefaultActions: 'true' },
+		{ label: 'Rate', fieldName: 'Rate__c', type: 'number', hideDefaultActions: 'true' },
+		{ label: 'Date Booked', fieldName: 'CreatedDate', type: 'date', hideDefaultActions: 'true' }
+	];
+
+	@track data = [];
+	@wire(tradeController)
+	wiredTrades({ error, data }) {
+		if (data) {
+			this.data = data;
+		}
+	}
+}
